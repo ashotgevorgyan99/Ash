1b665e4c6dcc0566925d07b7fdfcaa9ecdbcd1bb
ashotgevorgyan99 <150271993+ashotgevorgyan99@users.noreply.github.com>
2024-06-10 12:23:50 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     const couponButton = document.getElementById('coupons');
     const menuButton = document.getElementById('menu');
@@ -224,3 +223,35 @@ document.addEventListener('DOMContentLoaded', function() {
         buySetModal.style.display = 'none';
     });
 });
+
+
+
+
+
+
+
+// Get the modal
+var newsModal = document.getElementById("news-modal");
+
+// Get the button that opens the modal
+var newsBtn = document.getElementById("news-modal-button");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close-button-news")[0];
+
+// When the user clicks the button, open the modal 
+newsBtn.onclick = function() {
+    newsModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    newsModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == newsModal) {
+        newsModal.style.display = "none";
+    }
+            }
